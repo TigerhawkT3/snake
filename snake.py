e2d80f435fc571d4d27f4cbf7aebddb3234038a4
David Muller <david@mullerhome.com>
2018-04-28 02:56:10 -0700

    Finish debugging and implementing the complete game.

--- snake.py
+++ snake.py
@@ -1,6 +1,15 @@
 import tkinter as tk
-import collections.deque as dq
+from collections import deque as dq
 import random
+
+class Cell:
+    def __init__(self, x, y, square):
+        self.x = x
+        self.y = y
+        self.square = square
+    def __iter__(self):
+        yield self.x
+        yield self.y
 
 class Snake:
     def __init__(self, parent):
@@ -8,33 +17,46 @@ class Snake:
         self.WIDTH = 800
         self.HEIGHT = 800
         self.CELL = 50
-        self.RATE = 1000
+        self.max_speed_score = 1000 # stop increasing speed at this score
+        self.speed_factor = 20 # increase speed every x points
         self.canvas = tk.Canvas(root, width=self.WIDTH, height=self.HEIGHT)
-        self.parent.bind('<Ctrl-n>', self.draw_board)
+        self.canvas.pack()
+        self.parent.bind('<Control-n>', self.draw_board)
         self.parent.bind('<Left>', self.tick)
         self.parent.bind('<Right>', self.tick)
         self.parent.bind('<Up>', self.tick)
         self.parent.bind('<Down>', self.tick)
         self.draw_board()
-    def draw_board(self):
+    def draw_board(self, event=None):
         self.direction = 'Right'
+        self.tickrate = 1000
+        self.score = 1
         self.canvas.delete(tk.ALL)
-        self.squares = {(x, y):self.canvas.create_rectangle(x, y, x+self.CELL, y+self.CELL)
-                            for x in range(0, self.WIDTH, self.CELL)
-                            for y in range(0, self.HEIGHT, self.CELL)}
-        self.empty = set(self.squares)
-        coord = (self.WIDTH//2, self.HEIGHT//2)
+        self.empty = [(x, y) for x in range(0, self.WIDTH, self.CELL)
+                            for y in range(0, self.HEIGHT, self.CELL)]
+        x, y = self.empty.pop(len(self.empty)//2 + int(0.5 * len(self.empty)**0.5))
         # the head is 0 and the tail is -1
-        self.snake = dq([coord])
-        self.canvas.itemconfig(self.squares[coord], fill='black')
+        self.snake = dq([Cell(x, y, self.canvas.create_rectangle(x,
+                                                                 y,
+                                                                 x+self.CELL,
+                                                                 y+self.CELL,
+                                                                 fill='blue'))])
         self.create_food()
-        self.ticking = self.parent.after(self.RATE, self.tick)
+        self.ticking = self.parent.after(self.tickrate, self.tick)
+        self.running = True
     def create_food(self):
-        self.food = random.choice(tuple(self.empty))
-        self.empty.remove(self.food)
-        self.canvas.itemconfig(self.squares[self.food], fill='red')
+        try:
+            x, y = self.empty.pop(random.randrange(len(self.empty)))
+        except ValueError:
+            self.end(f'You win!\nScore: {self.score}.')
+        else:
+            self.food = Cell(x, y, self.canvas.create_rectangle(x, y, x+self.CELL, y+self.CELL, fill='red'))
+            if self.score <= self.max_speed_score:
+                self.tickrate = 1000 // (self.score//self.speed_factor + 1)
     def move(self, direction):
-        x,y = self.snake[0]
+        head = self.snake[0]
+        tail = self.snake[-1]
+        x,y = head
         if direction == 'Left':
             target = x-self.CELL, y
         elif direction == 'Right':
@@ -44,12 +66,45 @@ class Snake:
         else:
             target = x, y+self.CELL
         # collision detection etc.
+        if target == tuple(self.food):
+            self.snake.appendleft(self.food)
+            self.score += 1
+            self.recolor()
+            self.create_food()
+            return
+        if target in self.empty or target == tuple(tail):
+            self.empty.append(tuple(tail))
+            self.empty.remove(target)
+            tail.x, tail.y = target
+            self.canvas.coords(tail.square, *target, *(a+self.CELL for a in target))
+            self.snake.appendleft(self.snake.pop())
+            self.recolor()
+            return
+        self.end(f'You lose.\nScore: {self.score}.')
+    def recolor(self):
+        self.canvas.itemconfig(self.snake[0].square, fill='blue')
+        if len(self.snake) > 1:
+            self.canvas.itemconfig(self.snake[1].square, fill='black')
     def tick(self, event=None):
         if event:
-            self.direction = event.keysym
+            direction = event.keysym
+            if {direction, self.direction} in ({'Left', 'Right'},
+                                               {'Up', 'Down'}):
+                return
+            self.direction = direction
             self.parent.after_cancel(self.ticking)
-        self.move(self.direction)
-        self.ticking = self.parent.after(self.RATE, self.tick)
+        if self.running:
+            self.move(self.direction)
+            self.ticking = self.parent.after(self.tickrate, self.tick)
+    def end(self, text):
+        self.parent.after_cancel(self.ticking)
+        self.running = False
+        self.canvas.create_text(self.WIDTH//2,
+                                self.HEIGHT//2,
+                                text=text,
+                                font=('Times', self.HEIGHT//10),
+                                fill='orange')
+    
     
     
     
